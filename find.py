e8297e88622122ef06f442d7de3cc60640b83f99
akshay4 <akshay.shekhawat4@gmail.com>
2018-06-15 14:01:42 +0530

    new programs

---
+++ find.py
@@ -0,0 +1,29 @@
+a = "yolo is life man"
+x = a.find ("yolo")
+print(x)
+y = a.find("life")
+print(y)
+z = a.replace ("yolo","YOLO")
+print(z)
+c = a.count("yolo")
+print(c)
+w = a.count("man")
+print(w)
+sm = a.partition('Yolo')
+ms = a.split('yolo')
+print(sm)
+print(ms)
+f = a.replace("yolo","GG")
+print(f)
+i = "we are here to learn python"
+t = i.split() #by default split string from spaces
+p = i.partition('World') #Will partition string from world
+print(t)
+print(p)
+abc = '-'.join(t)
+print(abc)
+s = "lol"
+k = '^--^'.join(s)
+print(k)
+s = "Hello Python"
+print(s)
